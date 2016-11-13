58d82baaf1e1916907dcbe24041ac123b5f87fb1
hermesvf <hermes.valenciano1996@gmail.com>
2016-11-13 19:13:35 +0100

    refactor

--- DocSim2.cc
+++ DocSim2.cc
@@ -11,6 +11,22 @@ void mostraResultat(const vector<vector< double > >& matriu) {
   }
 }
 
+void readFiles(vector<string>& mFiles) {
+  string file1 = "En el caso de que su baja se prolonga other siiiiiise hasta 2017, el alemán no estará en Mestalla contra el Valencia, encuentro de Liga tras la disputa del Mundial de Clubes, que finaliza el 18 de diciembre.";
+  string file2 = "alemán adio José Alvalade (5ª jornad other siiiiiia de la fase de grupos de la Champions), Spotural Leonesa (vuelta de la Copa del Rey), El Clásico ante el Barcelona en el Camp Nou, Borussia Dortmund en el Santiago Bernabéu (6ª y última jornada de la fase de grupos de la Champions) y Deportivo de la Coruña.En el caso de que su baja se prolongase hasta 2017, el alemán no estará en Mestalla contra el Valencia, encuentro de Liga tras la disputa del Mundial de Clubes, que finaliza el 18 de diciembre.";
+  string file3 = "Heeeeeeeello from the other siiiiiiide";
+  string file4 = "parapjsgnsjgfgfdhdfljg other siiiiii";
+  mFiles.push_back(file1);
+  mFiles.push_back(file3);
+  mFiles.push_back(file2);
+  mFiles.push_back(file4);
+}
+                           //no puede ser const porque escribo su referencia
+void getReferencesFromFiles(vector<string>& mFiles, vector<string*>& mFileReferences) {
+  for (int i = 0; i < mFiles.size(); ++i)
+    mFileReferences.push_back(&mFiles[i]);
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 3) {
     cout << "Usage: ./DocSim2.x k t" << endl;
@@ -18,18 +34,11 @@ int main(int argc, char *argv[]) {
   }
   unsigned int k = atoi(argv[1]);
   unsigned int t = atoi(argv[2]);
-  vector<string*> mFiles;
-
-  string file1 = "En el caso de que su baja se prolonga other siiiiiise hasta 2017, el alemán no estará en Mestalla contra el Valencia, encuentro de Liga tras la disputa del Mundial de Clubes, que finaliza el 18 de diciembre.";
-  string file2 = "alemán adio José Alvalade (5ª jornad other siiiiiia de la fase de grupos de la Champions), Spotural Leonesa (vuelta de la Copa del Rey), El Clásico ante el Barcelona en el Camp Nou, Borussia Dortmund en el Santiago Bernabéu (6ª y última jornada de la fase de grupos de la Champions) y Deportivo de la Coruña.En el caso de que su baja se prolongase hasta 2017, el alemán no estará en Mestalla contra el Valencia, encuentro de Liga tras la disputa del Mundial de Clubes, que finaliza el 18 de diciembre.";
-  string file3 = "Heeeeeeeello from the other siiiiiiide";
-  string file4 = "parapjsgnsjgfgfdhdfljg other siiiiii";
-  mFiles.push_back(&file1);
-  mFiles.push_back(&file3);
-  mFiles.push_back(&file2);  // No lo puedo poner en otra funcion porque al ser referencias, cuando la funcion
-  mFiles.push_back(&file4);  // acaba, la referencia se borra, al ser variables locales.
-
-  JaccApprox mJA(k,t,mFiles);
+  vector<string> mFiles;
+  readFiles(mFiles);
+  vector<string*> mFileReferences;
+  getReferencesFromFiles(mFiles,mFileReferences);
+  JaccApprox mJA(k,t,mFileReferences);
   vector<vector< double > > matriu(mFiles.size(),vector<double>(mFiles.size(),0.0));
   mJA.obtainJaccardApproximation(matriu);
   mostraResultat(matriu);
